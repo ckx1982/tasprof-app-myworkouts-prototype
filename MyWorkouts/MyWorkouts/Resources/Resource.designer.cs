b1e60449ed68f197737fc9d085c48de4f82668bd
Karabaglidis <Karabaglidis@KOL06WSTHV-7315>
2019-01-10 14:01:27 +0100

    windows commit

--- MyWorkouts/MyWorkouts/Resources/Resource.designer.cs
+++ MyWorkouts/MyWorkouts/Resources/Resource.designer.cs
@@ -2423,8 +2423,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080037
 			public const int SYM = 2131230775;
 			
-			// aapt resource value: 0x7f0800a2
-			public const int action0 = 2131230882;
+			// aapt resource value: 0x7f0800a4
+			public const int action0 = 2131230884;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2447,17 +2447,17 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f08009f
-			public const int action_container = 2131230879;
+			// aapt resource value: 0x7f0800a1
+			public const int action_container = 2131230881;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800a6
-			public const int action_divider = 2131230886;
+			// aapt resource value: 0x7f0800a8
+			public const int action_divider = 2131230888;
 			
-			// aapt resource value: 0x7f0800a0
-			public const int action_image = 2131230880;
+			// aapt resource value: 0x7f0800a2
+			public const int action_image = 2131230882;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2474,11 +2474,11 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800a1
-			public const int action_text = 2131230881;
+			// aapt resource value: 0x7f0800a3
+			public const int action_text = 2131230883;
 			
-			// aapt resource value: 0x7f0800af
-			public const int actions = 2131230895;
+			// aapt resource value: 0x7f0800b1
+			public const int actions = 2131230897;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2513,8 +2513,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800a3
-			public const int cancel_action = 2131230883;
+			// aapt resource value: 0x7f0800a5
+			public const int cancel_action = 2131230885;
 			
 			// aapt resource value: 0x7f08009e
 			public const int cardView1 = 2131230878;
@@ -2531,8 +2531,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800ab
-			public const int chronometer = 2131230891;
+			// aapt resource value: 0x7f0800ad
+			public const int chronometer = 2131230893;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2591,8 +2591,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int end_padder = 2131230897;
+			// aapt resource value: 0x7f0800b3
+			public const int end_padder = 2131230899;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2636,8 +2636,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int icon_group = 2131230896;
+			// aapt resource value: 0x7f0800b2
+			public const int icon_group = 2131230898;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
@@ -2651,8 +2651,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080090
 			public const int imageView1 = 2131230864;
 			
-			// aapt resource value: 0x7f0800ac
-			public const int info = 2131230892;
+			// aapt resource value: 0x7f0800ae
+			public const int info = 2131230894;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2678,14 +2678,14 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
+			// aapt resource value: 0x7f0800b6
+			public const int masked = 2131230902;
+			
+			// aapt resource value: 0x7f0800a7
+			public const int media_actions = 2131230887;
+			
 			// aapt resource value: 0x7f0800b4
-			public const int masked = 2131230900;
-			
-			// aapt resource value: 0x7f0800a5
-			public const int media_actions = 2131230885;
-			
-			// aapt resource value: 0x7f0800b2
-			public const int message = 2131230898;
+			public const int message = 2131230900;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2699,14 +2699,14 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f08008f
 			public const int navigation = 2131230863;
 			
-			// aapt resource value: 0x7f0800b6
-			public const int navigation_dashboard = 2131230902;
+			// aapt resource value: 0x7f0800b8
+			public const int navigation_dashboard = 2131230904;
 			
 			// aapt resource value: 0x7f080098
 			public const int navigation_header_container = 2131230872;
 			
-			// aapt resource value: 0x7f0800b5
-			public const int navigation_home = 2131230901;
+			// aapt resource value: 0x7f0800b7
+			public const int navigation_home = 2131230903;
 			
 			// aapt resource value: 0x7f08003b
 			public const int never = 2131230779;
@@ -2717,14 +2717,14 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int notification_background = 2131230894;
+			// aapt resource value: 0x7f0800b0
+			public const int notification_background = 2131230896;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int notification_main_column = 2131230888;
+			// aapt resource value: 0x7f0800aa
+			public const int notification_main_column = 2131230890;
 			
-			// aapt resource value: 0x7f0800a7
-			public const int notification_main_column_container = 2131230887;
+			// aapt resource value: 0x7f0800a9
+			public const int notification_main_column_container = 2131230889;
 			
 			// aapt resource value: 0x7f08004c
 			public const int parallax = 2131230796;
@@ -2747,14 +2747,17 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080075
 			public const int radio = 2131230837;
 			
+			// aapt resource value: 0x7f0800a0
+			public const int recyclerView1 = 2131230880;
+			
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800ad
-			public const int right_icon = 2131230893;
+			// aapt resource value: 0x7f0800af
+			public const int right_icon = 2131230895;
 			
-			// aapt resource value: 0x7f0800a9
-			public const int right_side = 2131230889;
+			// aapt resource value: 0x7f0800ab
+			public const int right_side = 2131230891;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2858,8 +2861,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800a4
-			public const int status_bar_latest_event_content = 2131230884;
+			// aapt resource value: 0x7f0800a6
+			public const int status_bar_latest_event_content = 2131230886;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2885,6 +2888,9 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080069
 			public const int textSpacerNoTitle = 2131230825;
 			
+			// aapt resource value: 0x7f08009f
+			public const int textView1 = 2131230879;
+			
 			// aapt resource value: 0x7f08009d
 			public const int text_input_password_toggle = 2131230877;
 			
@@ -2894,8 +2900,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800aa
-			public const int time = 2131230890;
+			// aapt resource value: 0x7f0800ac
+			public const int time = 2131230892;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2945,8 +2951,8 @@ namespace Tasprof.App.MyWorkouts
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int visible = 2131230899;
+			// aapt resource value: 0x7f0800b5
+			public const int visible = 2131230901;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
@@ -3232,13 +3238,16 @@ namespace Tasprof.App.MyWorkouts
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int ic_launcher = 2130903040;
+			public const int ic_account_circle = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int ic_launcher_foreground = 2130903041;
+			public const int ic_launcher = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int ic_launcher_round = 2130903042;
+			public const int ic_launcher_foreground = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int ic_launcher_round = 2130903043;
 			
 			static Mipmap()
 			{
@@ -3372,6 +3381,12 @@ namespace Tasprof.App.MyWorkouts
 			
 			// aapt resource value: 0x7f0b0026
 			public const int status_bar_notification_info_overflow = 2131427366;
+			
+			// aapt resource value: 0x7f0b002b
+			public const int text = 2131427371;
+			
+			// aapt resource value: 0x7f0b002c
+			public const int text2 = 2131427372;
 			
 			// aapt resource value: 0x7f0b0029
 			public const int title_dashboard = 2131427369;
